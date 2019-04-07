1534fceb985598e76e26420f75a1d2cd6be1019b
Dallin parker <dallin.r.parker@icloud.com>
2019-04-06 23:37:43 -0400

    removed unneeded files and set up theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import React from 'react';
+import { theme } from './theme';
+import { render } from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { GlobalStyles } from './theme/globalStyles';
+import { ThemeProvider } from './theme/styledComponent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render(
+  <ThemeProvider theme={theme}>
+  <>
+    <App />
+    <GlobalStyles />
+  </>
+</ThemeProvider>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
